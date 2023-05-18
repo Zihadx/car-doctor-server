32de6a10aa4791db2ff1c672e60bcbb70c0f3875
zihadx <blankzihad@gmail.com>
2023-05-18 23:21:25 +0600

    vercel problem solve

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+// var jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_PASSWORD);
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4gdacpf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4gdacpf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,10 +24,112 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+const verifyJWT = (req,res, next)=>{
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error: true , message: ('unauthorize access')})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+    if(error){
+      return res.status(401).send({error: true, message: ('unauthorized access')})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //create database on mongodb
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const checkOutCollection = client.db("carDoctor").collection("checkOuts");
+
+    //jwt-------------------------
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
+
+    //for all data -----------------------
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //for specific one data ----------------------
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    //checkOut collection
+
+    app.post("/checkout", async (req, res) => {
+      const checkOut = req.body;
+      console.log(checkOut);
+      const result = await checkOutCollection.insertOne(checkOut);
+      console.log(result);
+      res.send(result);
+    });
+
+    //checkOut-------------- don't understand this step
+    app.get("/checkout",verifyJWT, async (req, res) => {
+      const decoded = req.decoded
+      console.log('came back after verify')
+      // console.log(req.headers.authorization);
+      if(decoded.email !==req.query.email){
+        return res.status(403).send({error:1, message: ('forbidden access')})
+      }
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await checkOutCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/checkout/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await checkOutCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put("/checkout/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateBooking = req.body;
+      console.log(updateBooking);
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await checkOutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +137,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
